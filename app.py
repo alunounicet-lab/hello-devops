4924dab4913eabb7893072ce67b02cacc9ec9a76
alunounicet-lab <alunounicet@gmail.com>
2026-05-05 20:25:26 -0300

    alteração da mensagem

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return ' Olá, DevOps, UNICET! '
+ return ' Olá, DevOps, ATIVIDADE CI-CD! '
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=10000)
