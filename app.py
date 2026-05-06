2789b18bafa9cbacb391029260d92f4884fa6858
alunounicet-lab <alunounicet@gmail.com>
2026-05-06 18:11:26 -0300

    Adicionado a palavra qualidade no print.

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return ' Olá, DevOps, Estou treinando CI-CD para ingressar no mercado de traabalho'
+ return ' Olá, DevOps, Estou treinando CI-CD para ingressar no mercado de trabalho, bem qualificado'
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=10000)
